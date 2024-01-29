3bcadb18982059af9d34239aaccb3f239c7311f9
Brad <44570552+bizojs@users.noreply.github.com>
2024-01-29 19:25:19 +0000

    feat: page transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 <script>
+  import Transition from "$lib/components/Transition.svelte"
   import "../app.css"
+
+  export let data
 </script>
 
 <div class="flex flex-col p-5">
-  <slot />
+  <Transition url={data.url}>
+    <slot />
+  </Transition>
 </div>
